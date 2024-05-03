93a61d6fb89e6de879f48cb159612b734b7a8a5a
Noam <noamkont@gmail.com>
2024-05-03 16:43:59 +0300

    Q_4 with target as Y and data as X

---
+++ main_Q4.py
@@ -0,0 +1,12 @@
+import pandas as pd
+from LinearRegression import LinearRegression
+import json
+from sklearn.datasets import fetch_california_housing
+
+if __name__ == "__main__":
+    housing = fetch_california_housing(as_frame=True)
+    linear_regression = LinearRegression()
+    data_train, data_test, target_train, target_test = LinearRegression.train_test_split(housing.data, housing.target)
+    linear_regression.fit(data_train, target_train)
+    score = linear_regression.score(data_test, target_test)
+    print(score)
