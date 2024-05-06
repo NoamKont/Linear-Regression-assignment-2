729d8e0ac19e4ccbb32a7242645bbf9ca367f095
Noam <noamkont@gmail.com>
2024-05-06 15:19:35 +0300

    Q_4 final

--- main_Q4.py
+++ main_Q4.py
@@ -4,10 +4,12 @@ import json
 from sklearn.datasets import fetch_california_housing
 
 if __name__ == "__main__":
-    housing = fetch_california_housing(as_frame=True)
+    housing = fetch_california_housing(as_frame=True)  # importing the house price as data frame
     linear_regression = LinearRegression()
-    data_train, data_test, target_train, target_test = LinearRegression.train_test_split(housing.data, housing.target)
+
+    data_train, data_test, target_train, target_test = LinearRegression.train_test_split(housing.data, housing.target)  # split the dataset train(80%) test(20%)
+
     linear_regression.fit(data_train, target_train)
-    score = linear_regression.score(data_test, target_test)
-    print(score)
+
     print("the weights are: ", linear_regression.weights_[1:], "the b is:", linear_regression.weights_[0])
+    print('The test score is:', linear_regression.score(data_test, target_test))
