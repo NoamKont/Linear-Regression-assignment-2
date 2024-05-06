e6ecf3e537bce2caf9c76a49e5bb3b5cce398661
Noam <noamkont@gmail.com>
2024-05-06 15:12:41 +0300

    Q_3 final

--- main_Q3.py
+++ main_Q3.py
@@ -1,5 +1,4 @@
 import json
-
 from LinearRegression import LinearRegression
 import numpy as np
 import pandas as pd
@@ -8,14 +7,13 @@ if __name__ == "__main__":
     with open('config.json', 'r', encoding='utf8') as json_file:
         config = json.load(json_file)
     df = pd.read_csv(config['data_file'])
-
-    X = df.drop('y', axis=1)  # Drop the diagnosis column from features
+    X = df.drop('y', axis=1)  # Drop the "y" column from features
     y = df['y']
+
     linear_regression = LinearRegression()
 
     X_train, X_test, y_train, y_test = LinearRegression.train_test_split(X, y)  # split the dataset
-    #linear_regression.fit(X_train, y_train)
-    linear_regression.fit(X.values, y.values)
-    #y_pred = linear_regression.predict(X_test)
-    print("the weights are: ", linear_regression.weights_[1:], "the b is:", linear_regression.weights_[0])
-    print(linear_regression.score(X.values, y.values))
+
+    linear_regression.fit(X_train, y_train)
+    print("The weights are: ", linear_regression.weights_[1:], "the b is:", linear_regression.weights_[0])
+    print("The score test is: ", linear_regression.score(X_test, y_test))
